38900763d6efb3df0a89f9f8d8156fb5d34a89af
AdrianHsu <adrianhsu1995@gmail.com>
2018-01-19 17:12:56 +0800

    :sparkles: button download png
    yay

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ function handleFileUpload(event){
   reader.readAsText(input.files[0]);
 }
 
+let png_src = "";
+
 function handleFileSelected(event) {
    var input = document.getElementById('input');
    var reader = new FileReader();
@@ -37,9 +39,9 @@ function handleFileSelected(event) {
         document.getElementById("image").innerHTML = 'Sorry.\nThe File is too big.';
         return;
      }
-
-     const image = Viz(digraph, { format: "svg" });
-     document.getElementById("image").innerHTML = image;
+     var svgXml = Viz(digraph, { format: "svg" });
+     tmp = Viz.svgXmlToPngImageElement(svgXml);
+     document.getElementById("image").innerHTML = svgXml;
    };
    reader.onprogress = function(){
      //FIXME spinning icon when loading
@@ -47,5 +49,35 @@ function handleFileSelected(event) {
    reader.readAsText(input.files[0]);
 };
 
+
+function downloadPng() {
+    var img = png_src;
+// ref: https://stackoverflow.com/questions/10473932/browser-html-force-download-of-image-from-src-dataimage-jpegbase64
+    var image_data = atob(img.src.split(',')[1]);
+    // Use typed arrays to convert the binary data to a Blob
+    var arraybuffer = new ArrayBuffer(image_data.length);
+    var view = new Uint8Array(arraybuffer);
+    for (var i=0; i<image_data.length; i++) {
+        view[i] = image_data.charCodeAt(i) & 0xff;
+    }
+    try {
+        // This is the recommended method:
+        var blob = new Blob([arraybuffer], {type: 'application/octet-stream'});
+    } catch (e) {
+        // The BlobBuilder API has been deprecated in favour of Blob, but older
+        // browsers don't know about the Blob constructor
+        // IE10 also supports BlobBuilder, but since the `Blob` constructor
+        //  also works, there's no need to add `MSBlobBuilder`.
+        var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
+        bb.append(arraybuffer);
+        var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
+    }
+
+    // Use the URL object to create a temporary URL
+    var url = (window.webkitURL || window.URL).createObjectURL(blob);
+    location.href = url; // <-- Download!
+}
+
+document.getElementById('foo').addEventListener('click', downloadPng);
 document.getElementById('input').addEventListener('change', handleFileUpload);
 document.getElementById('btn').addEventListener('click', handleFileSelected);
